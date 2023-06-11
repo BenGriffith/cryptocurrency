13e5cbc25ee136e0de8563eb093a7ce80be9bd98
Ben Griffith <bengriffith@outlook.com>
2023-06-11 04:31:56 +0000

    updated loader module and added transformer module

--- src/crypto/loader.py
+++ src/crypto/loader.py
@@ -3,28 +3,35 @@ from typing import Dict
 from datetime import datetime
 
 from decouple import config
-from google.cloud.storage import Client as GCSClient
-from google.cloud.bigquery import Client as BQClient
+from google.cloud.storage import Client
+from google.cloud.storage.bucket import Bucket
+
+from utils.api import Connection
+from utils.constants import LISTINGS_LATEST_URL
 
 
 class Loader:
 
     def __init__(self) -> None:
-        self._gcs_client = GCSClient()
-        self._bq_client = BQClient()
+        self._gcs_client = Client()
     
-    def _get_bucket(self) -> str:
+    def _get_bucket(self) -> Bucket:
         bucket_name = config("BUCKET", cast=str)
         bucket = self._gcs_client.bucket(bucket_name)
         return bucket
 
-    def create_blob(self, data: Dict) -> None:
+    def create_blob(self, crypto_data: Dict) -> None:
         bucket = self._get_bucket()
         blob_name = f"{datetime.today().strftime('%Y-%m-%d')}.json"
         blob = bucket.blob(blob_name)
-
         with blob.open("w") as file:
-            file.write(json.dumps(data))
+            file.write(json.dumps(crypto_data))
 
-    def load_tables():
-        pass
+
+if __name__ == "__main__":
+    connection = Connection()
+    coinmarket_response = connection.request(url=LISTINGS_LATEST_URL)
+    data = coinmarket_response["data"]
+
+    loader = Loader()
+    loader.create_blob(crypto_data=coinmarket_response)
