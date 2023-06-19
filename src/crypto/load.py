aab1754796de2f7c9b3429a841f55ddaba66473a
Ben Griffith <bengriffith@outlook.com>
2023-06-19 06:16:09 +0000

    updated helpers and load

--- src/crypto/load.py
+++ src/crypto/load.py
@@ -1,26 +1,34 @@
 import json
-from typing import Dict
 from datetime import datetime
+
+from google.cloud.storage import Client
+from google.cloud.storage.bucket import Bucket
+from google.cloud.storage.blob import Blob
 
 from crypto.utils.api import Connection
 from crypto.utils.constants import (
     LISTINGS_LATEST_URL, 
     CLOUD_STORAGE, 
-    BUCKET, 
-    Services,
+    BUCKET,
 )
-from crypto.utils.helpers import client
+from crypto.utils.helpers import service_credentials
 
 
 class Load:
 
-    def __init__(self, service: str, service_acct: dict, bucket: str) -> None:
-        self.client = client(service=service, service_acct=service_acct)
-        self.bucket = self.client.bucket(bucket)
+    def __init__(self, service_acct: dict, bucket_name: str) -> None:
+        self.client = Client(credentials=service_credentials(service_acct))
+        self.bucket_name = bucket_name
 
-    def create_blob(self, crypto_data: Dict) -> None:
+    def bucket(self) -> Bucket:
+        return self.client.bucket(bucket_name=self.bucket_name)
+    
+    def blob(self, blob_name: str) -> Blob:
+        return self.bucket().blob(blob_name=blob_name)
+
+    def create_blob(self, crypto_data: dict) -> None:
         blob_name = f"{datetime.today().strftime('%Y-%m-%d')}.json"
-        blob = self.bucket.blob(blob_name)
+        blob = self.blob(blob_name=blob_name)
         with blob.open("w") as file:
             file.write(json.dumps(crypto_data))
 
@@ -29,9 +37,5 @@ if __name__ == "__main__":
     connection = Connection()
     coinmarket_response = connection.request(url=LISTINGS_LATEST_URL)
 
-    load = Load(
-        service=Services.GCS.value,
-        service_acct=CLOUD_STORAGE,
-        bucket=BUCKET,
-    )
+    load = Load(service_acct=CLOUD_STORAGE, bucket_name=BUCKET)
     load.create_blob(crypto_data=coinmarket_response)
--- src/crypto/utils/constants.py
+++ src/crypto/utils/constants.py
@@ -1,10 +1,5 @@
 import os
-from enum import Enum
 
 LISTINGS_LATEST_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 CLOUD_STORAGE = os.getenv("CLOUD_STORAGE")
 BUCKET = os.getenv("BUCKET")
-
-class Services(Enum):
-    GCS = "storage"
-    BQ = "bigquery"
