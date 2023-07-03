2928a795ff4b4352e57161b027dfda9ffbd1f8ff
Ben Griffith <bengriffith@outlook.com>
2023-07-03 04:01:43 +0000

    added fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
 import pytest
+import calendar
 from unittest.mock import MagicMock
 
 from google.cloud.storage import Client
 from google.cloud.storage.bucket import Bucket
 from google.cloud.storage.blob import Blob
+from google.cloud.bigquery import Client as BQClient
+
+from crypto.utils.helpers import time_period
+
+
+DAYS = "Monday Tuesday Wednesday Thursday Friday Saturday Sunday".split(" ")
+MONTHS = "January February March April May June July August September October November December".split(" ")
 
 
 @pytest.fixture
@@ -71,3 +79,44 @@ def mock_bucket():
 @pytest.fixture
 def mock_blob():
     return MagicMock(spec=Blob)
+
+
+@pytest.fixture
+def mock_bq_client():
+    return MagicMock(spec=BQClient)
+
+
+@pytest.fixture
+@time_period
+def time_period_days():
+    day_names = list(calendar.day_name)
+    return day_names   
+
+
+@pytest.fixture
+def days_keys_names(time_period_days):
+    rows = time_period_days
+    keys, names = [], []
+    for row in rows:
+        key, name = row
+        keys.append(key)
+        names.append(name)
+    return keys, names
+
+
+@pytest.fixture
+def months_keys_names(time_period_months):
+    rows = time_period_months
+    keys, names = [], []
+    for row in rows:
+        key, name = row
+        keys.append(key)
+        names.append(name)
+    return keys, names
+
+
+@pytest.fixture
+@time_period
+def time_period_months():
+    month_names = list(calendar.month_name)[1:]
+    return month_names
