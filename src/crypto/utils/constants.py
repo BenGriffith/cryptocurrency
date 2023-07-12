0c5d1d53b2672a7f1dcb34a1bf14b3551b13bb53
Ben Griffith <bengriffith@outlook.com>
2023-07-12 13:23:44 +0000

    added DATE_DIM

--- src/crypto/utils/constants.py
+++ src/crypto/utils/constants.py
@@ -15,4 +15,5 @@ PROJECT_ID = config("PROJECT_ID", cast=str)
 DATASET = config("DATASET", cast=str)
 DAY_DIM = f"{PROJECT_ID}.{DATASET}.{config('DAY_DIM', cast=str)}"
 MONTH_DIM = f"{PROJECT_ID}.{DATASET}.{config('MONTH_DIM', cast=str)}"
+DATE_DIM = f"{PROJECT_ID}.{DATASET}.{config('DATE_DIM', cast=str)}"
 INITIAL_LOAD = config("INITIAL_LOAD", cast=bool)
